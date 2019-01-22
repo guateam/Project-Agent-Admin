f6f1bcb3d1866e80bc6307647af0fe7b4217cb79
laipz8200 <laipz8200@icloud.com>
2019-01-22 20:54:09 +0800

    login

--- src/api/sys/login/index.js
+++ src/api/sys/login/index.js
@@ -1,6 +1,8 @@
 import request from '@/plugin/axios'
+import qs from 'qs'
 
 export function AccountLogin (data) {
+  data = qs.stringify(data)
   return request({
     url: '/account/login',
     method: 'post',
