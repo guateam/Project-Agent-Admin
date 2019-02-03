d4532f9e46e4eeee9c62c4d146e180f15041685b
hanerx <zhangyu199946@gmail.com>
2019-02-03 12:54:49 +0800

    对接

--- src/pages/answer/components/TabTable/TabTable.vue
+++ src/pages/answer/components/TabTable/TabTable.vue
@@ -1,110 +1,136 @@
 <template>
-  <div class="tab-table">
-    <basic-container>
-      <el-tabs v-model="tabKey" @tab-click="handleClick">
-        <el-tab-pane
-          v-for="tab in tabs"
-          :label="tab.tab"
-          :name="tab.key"
-          :key="tab.key">
-          <el-table
-            :data="dataSource[tab.key]"
-            style="width: 100%">
-            <el-table-column
-              v-for="item, index in columns"
-              :label="item.title"
-              :prop="item.dataIndex"
-              :key="item.key"
-              :width="item.key !== 'action' ? (item.width || 160) : item.width">
-              <template slot-scope="scope">
-                <span v-if="item.key !== 'action'">{{scope.row[item.dataIndex]}}</span>
-                <edit-dialog :row="scope.row" :key.sync="item.key" :index="scope.$index" :tabKey="tabKey" @handleMod="handleMod"></edit-dialog>
-                <delete-balloon :key.sync="item.key" :index="scope.$index" :tabKey="tabKey" @handleRemove="handleRemove"></delete-balloon>
-              </template>
-            </el-table-column>
-          </el-table>
-        </el-tab-pane>
-      </el-tabs>
-    </basic-container>
-  </div>
+    <div class="tab-table">
+        <basic-container>
+            <el-tabs v-model="tabKey" @tab-click="handleClick">
+                <el-tab-pane
+                        v-for="tab in tabs"
+                        :label="tab.tab"
+                        :name="tab.key"
+                        :key="tab.key">
+                    <el-table
+                            :data="dataSource[tab.key]"
+                            style="width: 100%">
+                        <el-table-column
+                                v-for="item, index in columns"
+                                :label="item.title"
+                                :prop="item.dataIndex"
+                                :key="item.key"
+                                :width="item.key !== 'action' ? (item.width || 160) : item.width">
+                            <template slot-scope="scope">
+                                <span v-if="item.key !== 'action'">{{scope.row[item.dataIndex]}}</span>
+                                <!--<edit-dialog :row="scope.row" :key.sync="item.key" :index="scope.$index"-->
+                                             <!--:tabKey="tabKey" @handleMod="handleMod"></edit-dialog>-->
+                                <delete-balloon :row="scope.row" :key.sync="item.key" :index="scope.$index" :tabKey="tabKey"
+                                                @handleRemove="handleRemove"></delete-balloon>
+                            </template>
+                        </el-table-column>
+                    </el-table>
+                </el-tab-pane>
+            </el-tabs>
+        </basic-container>
+    </div>
 </template>
 
 <script>
-import BasicContainer from '@vue-materials/basic-container';
-import DeleteBalloon from './components/DeleteBalloon';
-import EditDialog from './components/EditDialog';
-import response from './tab-table.json';
+    import BasicContainer from '@vue-materials/basic-container';
+    import DeleteBalloon from './components/DeleteBalloon';
+    import EditDialog from './components/EditDialog';
+    import response from './tab-table.json';
+    import request from '@/plugin/axios'
 
-export default {
-  components: {
-    BasicContainer,
-    DeleteBalloon,
-    EditDialog,
-  },
-  name: 'TabTable',
+    export default {
+        components: {
+            BasicContainer,
+            DeleteBalloon,
+            EditDialog,
+        },
+        name: 'TabTable',
 
-  data() {
-    return {
-      tabKey: 'all',
-      dataSource: [],
-      tabs: [
-        { tab: '全部', key: 'all' },
-        { tab: '已审核', key: 'inreview' },
-        { tab: '已处理', key: 'rejected' },
-      ],
-      columns: [
-        {
-          title: '回答',
-          dataIndex: 'title',
-          key: 'title',
+        data() {
+            return {
+                tabKey: 'all',
+                dataSource: [],
+                tabs: [
+                    {tab: '全部', key: 'all'},
+                    {tab: '待审核', key: 'wait'},
+                    {tab: '已清除', key: 'deleted'},
+                ],
+                columns: [
+                    {
+                        title: '回答',
+                        dataIndex: 'content',
+                        key: 'title',
+                        width:400
+                    },
+                    {
+                        title: '回答者',
+                        dataIndex: 'nickname',
+                        key: 'author',
+                    },
+                    {
+                        title: '问题',
+                        dataIndex: 'title',
+                        key: 'status',
+                        width:400
+                    },
+                    {
+                        title: '状态',
+                        dataIndex: 'status',
+                        key: 'status',
+                    },
+                    {
+                        title: '发布时间',
+                        dataIndex: 'edittime',
+                        key: 'date',
+                    },
+                    {
+                        title: '操作',
+                        key: 'action',
+                    },
+                ],
+                visible: false,
+            };
         },
-        {
-          title: '回答者',
-          dataIndex: 'author',
-          key: 'author',
-        },
-        {
-          title: '问题',
-          dataIndex: 'status',
-          key: 'status',
-        },
-        {
-          title: '发布时间',
-          dataIndex: 'date',
-          key: 'date',
-        },
-        {
-          title: '操作',
-          key: 'action',
-        },
-      ],
-      visible: false,
-    };
-  },
 
-  created() {},
+        created() {
+        },
 
-  mounted() {
-    this.dataSource = response.data;
-  },
+        mounted() {
+            this.get_list()
+        },
 
-  methods: {
-    handleClick(tab) {
-      console.log(tab);
-    },
-    handleRemove(index, tabKey) {
-      this.dataSource[tabKey].splice(index, 1);
-    },
-    handleMod(row, index, tabKey) {
-      this.$set(this.dataSource[tabKey], index, row);
-    },
-  },
-}
+        methods: {
+            get_list() {
+                request({
+                    url: '/answer/back_get_answers',
+                    method: 'get'
+                }).then(res => {
+                    window.console.log(res);
+                    // 下面一行是替换表格内容为后台数据，接口确认无误后应取消注释
+                    this.dataSource = res
+                })
+            },
+            handleClick(tab) {
+                console.log(tab);
+            },
+            handleRemove(row) {
+                request({
+                    url: '/answer/back_get_answers?answer_id='+row['answer_id'],
+                    method: 'get'
+                }).then(() => {
+                    this.get_list()
+                })
+            },
+            handleMod(row, index, tabKey) {
+                this.$set(this.dataSource[tabKey], index, row);
+            },
+        },
+    }
 
 </script>
 
 <style>
-  .tab-table {
+    .tab-table {
 
-  }
+    }
 </style>
--- src/pages/notice/Notice.vue
+++ src/pages/notice/Notice.vue
@@ -1,9 +1,9 @@
 <template>
   <!-- d2-container 文档 https://doc.d2admin.fairyever.com/zh/sys-components/container.html -->
   <d2-container class="page-notice">
-    <template slot="header">notice</template>
+    <template slot="header">系统通知</template>
     <basic-form/>
-    <template slot="footer">自动生成 1 个组件</template>
+    <template slot="footer">联系我们 © 吃瓜小队</template>
   </d2-container>
 </template>
 
